78bf9141297332030b146d895a6d0d7a2a144cc5
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-22 01:27:46 +0700

    Test emit

--- src/App.fs
+++ src/App.fs
@@ -3,6 +3,31 @@ module Fable10
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Browser
+
+[<Emit("undefined")>]
+let undefined: obj = jsNative
+
+[<Emit("1")>]
+let one: int = jsNative
+
+[<Emit("$0 === undefined")>]
+let isUndefined(x: 'a) : bool = jsNative
+
+[<Emit("isNaN(parseFloat($0)) ? null : parseFloat($0)")>]
+let parseFloat (input: string) : float option = jsNative
+
+let test() =
+    console.log("Hello, world!")
+    console.log(undefined)
+    console.log( one + one + one)
+    console.log(isUndefined 5)
+    console.log(isUndefined "")
+    console.log(isUndefined undefined)
+
+    match parseFloat "335.83" with
+    | Some value -> console.log(value)
+    | None -> console.log("parseFloat failed")
 
 let init() =
     let canvas = Browser.document.getElementsByTagName_canvas().[0]
@@ -16,4 +41,5 @@ let init() =
     ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
     ctx.fillRect (30., 30., 55., 50.)
 
+test()
 init()
